e122399fb046888ba2aca45cb21396d9319408fd
user1 <user1@SALMA-THASNI>
2025-10-31 01:21:01 +0530

    commit messege1

--- SeleniumProject/src/main/java/testscript2/TableHandling.java
+++ SeleniumProject/src/main/java/testscript2/TableHandling.java
@@ -8,57 +8,57 @@ import org.openqa.selenium.WebElement;
 import testscript.Base;
 
 public class TableHandling extends Base{
-	//Full table printing
+	//to automate a full table
 	void tablePrinting() {
 		driver.navigate().to("https://selenium.qabible.in/table-pagination.php");
 		WebElement tableprinting=driver.findElement(By.xpath("//table[@id='dtBasicExample']"));
 		System.out.println(tableprinting.getText());
 	}
-	//Printing only a row
+	
+	//to automate  a row
 	void rowPrinting() {
 		driver.navigate().to("https://selenium.qabible.in/table-pagination.php");
-		//tbody and tr from inpect
-		WebElement rowprinting=driver.findElement(By.xpath("//table[@id='dtBasicExample']//tbody//tr[1]"));
+		WebElement rowprinting=driver.findElement(By.xpath("//table[@id='dtBasicExample']//tbody//tr[1]")); //tbody and tr from inpect
 		System.out.println(rowprinting.getText());
 	}
-	//Printing 3rd value data from the first row
+	
+	//automate 3rd value data from the first row
 	void cellPrinting() {
-		driver.navigate().to("https://selenium.qabible.in/table-pagination.php");
-		//td - thread data
+		driver.navigate().to("https://selenium.qabible.in/table-pagination.php"); //td - table data
 		WebElement cellprinting=driver.findElement(By.xpath("//table[@id='dtBasicExample']//tbody//tr[1]//td[3]"));
 		System.out.println(cellprinting.getText());
 	}
-	//Printing only first Name column data 
+	
+	//automate only the first Name in the column  
 	void columnPrinting() {
 		driver.navigate().to("https://selenium.qabible.in/table-pagination.php");
 		//tr - select all tr for column printing because all values are given as row details
 		//list of elements are printing - findElements included in list
-		List<WebElement> rowprinting=driver.findElements(By.xpath("//table[@id='dtBasicExample']//tbody//tr//td[1]"));
-		for(WebElement list:rowprinting) {
+		List<WebElement> columnprinting = driver.findElements(By.xpath("//table[@id='dtBasicExample']//tbody//tr//td[1]"));
+		for(WebElement list : columnprinting) {
 			System.out.println(list.getText());
 		}
 	}
 	//Checking whether Ashton Cox Name is present in the table or not
-	void checkNamePresentOrNot() {
+	void checkingName() {
 		driver.navigate().to("https://selenium.qabible.in/table-pagination.php");
-		List<WebElement> checkprinting=driver.findElements(By.xpath("//table[@id='dtBasicExample']//tbody//tr//td[1]"));
-		String name="Ashton Cox";
-		for (WebElement n : checkprinting) {
-            if (n.getText().equalsIgnoreCase(name)) {
-                System.out.println(name + " is present under the Name field of the table.");
+		List<WebElement> checkprinting = driver.findElements(By.xpath("//table[@id='dtBasicExample']//tbody//tr//td[1]"));
+		String name = "Ashton Cox";
+		for (WebElement n1 : checkprinting) {
+            if (n1.getText().equalsIgnoreCase(name)) {
+                System.out.println(name + " is present in the table.");
             }
         }
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		TableHandling tablehandling=new TableHandling();
 		tablehandling.browserInitialization();
 		//tablehandling.tablePrinting();
 		//tablehandling.rowPrinting();
 		//tablehandling.cellPrinting();
 		//tablehandling.columnPrinting();
-		tablehandling.checkNamePresentOrNot();
+		tablehandling.checkingName();
 		
 		//tablehandling.browserQuitAndClose();
 	}
